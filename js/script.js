785b78bb8b46d10f25631a888736baf46b27bc70
Benny <bbxblack@gmail.com>
2021-04-15 15:25:15 +0200

    v0.3

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
 // Attraverso l'apposita API di Boolean,
 // generare 10 indirizzi email e
 // stamparli in pagina all'interno di una lista.
+
+function mailGenerator() {
+    $.ajax({
+        url: 'https://flynn.boolean.careers/exercises/api/random/mail',
+        method: 'GET',
+        success: function(data) {
+            const res = data.response;
+            console.log(res);
+            generateMail(res);
+        },
+        error: function() {
+            console.log('error');
+        }
+    });
+
+    function generateMail(string) {
+
+      const target = $('#list');
+
+      target.append('<ul>' + string + '</ul>')
+
+    };
+
+function init() {
+   mailGenerator();
+}
+document.addEventListener('DOMContentLoaded', init);
